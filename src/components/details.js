5756c716a7da2cd34e39e78ce35ce0acd42b49d8
michal <859michal@gmail.com>
2022-07-27 12:38:09 +0300

    m

--- src/components/details.js
+++ src/components/details.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import Navigation from "./navigation";
 
 const axios = require('axios').default;
 
 const url = new URL('https://meetings-test.herokuapp.com/business/d1d6012c-d5d5-4dae-afd9-1e8e5ddb27ba')
 
-const getDetails = () =>async() => {
+const getDetails = async() => {
     try {
         const response = await axios.get(url);
         console.log(response);
@@ -24,5 +25,5 @@ const Details = () => {
     )
 }
 
-export default Home;
+export default Details;
 
