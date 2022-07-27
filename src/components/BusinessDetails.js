dfa3152844d012f9c2f7744ee6825277baac6ead
RuchamiBerenshtein <r9743740@gmail.com>
2022-07-27 14:17:52 +0300

    routts is work :)

---
+++ src/components/BusinessDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessDetails = () => {
+    return (
+        <div>
+            <div>BusinessDetails!!!</div>
+        </div>
+    )
+}
+
+export default BusinessDetails;
--- src/components/details.js
+++ src/components/details.js
@@ -1,28 +1,29 @@
-import Navigation from "./navigation";
+import React from 'react';
 
-const axios = require('axios').default;
+import Navigation from './Navigation';
 
-const url = new URL('https://meetings-test.herokuapp.com/business/d1d6012c-d5d5-4dae-afd9-1e8e5ddb27ba')
+// const axios = require('axios').default;
 
-const getDetails = () =>async() => {
-    try {
-        const response = await axios.get(url);
-        console.log(response);
-    } catch (error) {
-        console.error(error);
-    }
-}
+// const url = new URL('https://meetings-test.herokuapp.com/business/d1d6012c-d5d5-4dae-afd9-1e8e5ddb27ba')
+
+// const getDetails = () =>async() => {
+//     try {
+//         const response = await axios.get(url);
+//         console.log(response);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
 const Details = () => {
     return (
         <div>
             <Navigation />
-            <br></br>
             <div>Details!!!</div>
         </div>
 
     )
 }
 
-export default Home;
+export default Details;
 
