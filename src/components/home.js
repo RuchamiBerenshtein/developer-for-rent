4d792c0b9e86452c86968bef6cf1c5fdc7bda6ae
michal <859michal@gmail.com>
2022-07-28 17:51:43 +0300

    m

--- src/components/home.js
+++ src/components/home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Navigation from "./navigation";
 import '../css/home.css'
 
-const Home = () => {
+
+const Home =() => {
     return (
-        <div className="body">
+        <div className="body" >
             <Navigation className="navigation"/>
             <h1 className="welcome-title">Welcome to Programer4Rent</h1>
         </div>
