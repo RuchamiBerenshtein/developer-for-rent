43af29fea266cb336c88ea755c98e687b6205be7
michal <859michal@gmail.com>
2022-07-28 13:44:31 +0300

    try

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React,{useState} from 'react';
 import userIcon from '../imgs/icons/user-icon.svg';
 import '../css/login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const signIn=()=>{
+const axios = require('axios').default;
+const url = new URL('https://meetings-test.herokuapp.com/user/signin')
+
+
+const signIn=(email,password) =>{
+    if(email === '' || password === ''){
+
+    }
+    else{
+         axios.post(url, {
+           username:email,
+           password:password
+          })
+          .then((response) =>{
+              return response.data;
+          })
+          .then((data) =>{
+            if(data.id){
+                window.location.href="http://localhost:3000/Manager";
+            }
+        })
+          .catch(function (error) {
+            return error;
+          });
+    }
     console.log('signed in!')
 }
 
+
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password,setPassword]=useState('');
+
     return (
         <div className="body">
             <div className="login-form">
@@ -19,13 +48,13 @@ const Login = () => {
                     <br></br>
                     <div>
                         <div className="form-group">
-                            <input type="email" className="form-control form-input" id="email" placeholder="Enter your Email address"></input>
+                            <input type="email" className="form-control form-input" id="email" placeholder="Enter your Email address" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control form-input" id="password" placeholder="Enter your Password"></input>
+                            <input type="password" className="form-control form-input" id="password" placeholder="Enter your Password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
                         </div>
                         <div className="form-group">
-                            <input type="button" className="form-control" id="login" value="Login as a manager" onClick={signIn}></input>
+                            <input type="button" className="form-control" id="login" value="Login as a manager" onClick={()=>signIn(email,password)}></input>
                         </div>
                     </div>
                 </div>
--- src/components/manager.js
+++ src/components/manager.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 
 const Manager = () => {
     return (
         <div>
             <div>Manager!!!</div>
+            <nav>
+                <Link to="/ServicesManagement">Services</Link> | {" "}
+                <Link to="/busimessDetails">business details</Link> | {" "}
+                <Link to="/schedule">schedule</Link>
+            </nav>
+            {/* <div className="content">
+                <Outlet/>
+            </div> */}
         </div>
     )
 }
